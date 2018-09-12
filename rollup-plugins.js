48266f16e82d49c24f0b934b3f9e4100d3985dfd
Pranav Gupta <pranavguptarulz@gmail.com>
2018-09-12 12:08:46 +0530

    allow linting to be specified

--- rollup-plugins.js
+++ rollup-plugins.js
@@ -6,17 +6,21 @@ const babelPlugins = require('./scripts/babel-plugins');
 const stylus = require('./scripts/rollup-plugin-stylus');
 const svelte = require('rollup-plugin-svelte');
 const inject = require('rollup-plugin-inject');
-const { lint, lintText } = require('./scripts/eslint');
+const eslint = require('./scripts/eslint');
 
 const isWatching = argv.w || argv.watch;
 
-const getPluginsWithShared = sharedDir => baseDir => {
-  if (!Array.isArray(baseDir)) {
-    baseDir = [baseDir];
-  }
-  const paths = baseDir.concat(sharedDir);
+const commonFeDir = 'node_modules/fe/src';
 
-  lint(isWatching)(paths);
+const getPlugins = ({ watch = isWatching, lint, src }) => {
+  if (!Array.isArray(src)) {
+    src = [src];
+  }
+  const paths = src.concat(commonFeDir);
+
+  if (lint) {
+    eslint.lint(isWatching)(paths);
+  }
 
   // Order of plugins is important:
   // svelte needs to be before babel so that by the time
@@ -29,7 +33,7 @@ const getPluginsWithShared = sharedDir => baseDir => {
       preprocess: {
         style: ({ content }) => stylus.transform(content),
         script: ({ content }) => {
-          setTimeout(() => lintText(content));
+          setTimeout(() => eslint.lintText(content));
           return content;
         },
       },
@@ -43,8 +47,6 @@ const getPluginsWithShared = sharedDir => baseDir => {
     inject(globals),
   ];
 };
-
-const pluginsWithDir = getPluginsWithShared('node_modules/fe/src');
 
 const rollupCommon = {
   treeshake: {
@@ -65,5 +67,5 @@ module.exports = {
   stylus,
   isWatching,
   rollupCommon,
-  pluginsWithDir,
+  getPlugins,
 };
