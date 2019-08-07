fec7f290b6ff5aa078d361ff4639b129686fd114
Umang Galaiya <umang.galaiya@razorpay.com>
2019-08-07 18:38:06 +0530

    Add _Arr.insertAt

--- tests/fe/implicit/_Arr.js
+++ tests/fe/implicit/_Arr.js
@@ -384,4 +384,24 @@ describe('_Arr', () => {
       deep(second, secondCopy);
     });
   });
+
+  describe('insertAt', () => {
+    const base = [1, 2, 3];
+    const first = [1, 4, 2, 3];
+    const second = [1, 2, 3, undefined, undefined, undefined, 4];
+
+    it('inserts within length', () => {
+      const returned = _Arr.insertAt(base, 4, 1);
+
+      deep(returned, first);
+      notDeep(returned, base);
+    });
+
+    it('inserts beyond length', () => {
+      const returned = _Arr.insertAt(base, 4, 6);
+
+      deep(returned, second);
+      notDeep(returned, base);
+    });
+  });
 });
