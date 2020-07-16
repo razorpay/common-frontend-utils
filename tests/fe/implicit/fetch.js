85376896d6a9b4dc0a3f7f8e114daad0de4e5d94
Umang Galaiya <umang.galaiya@razorpay.com>
2020-07-16 12:52:57 +0530

    Add test for fetch.jsonp

--- tests/fe/implicit/fetch.js
+++ tests/fe/implicit/fetch.js
@@ -50,39 +50,37 @@ describe('fetch', () => {
       });
     });
   });
-  // remaining
-  // describe('jsonp', () => {
-  //   it('Check if it sends jsonp request and gets a response for correct URL', function(done) {
-  //     global.Razorpay={};
-  //     fetch.jsonp({
-  //       url: 'https://api.razorpay.com/v1/preferences?key_id=rzp_test_1DP5mmOlF5G5ag',
-  //       callback: response => {
-  //           const hasValidFields=response.http_status_code&&response.features.google_pay&&response.mode==='test';
-  //           if(hasValidFields){
-  //               done();
-  //           }
-  //           else{
-  //               done(new Error());
-  //           }
-  //       },
-  //     });
-  //   });
 
-  //   it('Check if it sends jsonp request and gets a response for incorrect URL', function(done) {
-  //       global.Razorpay={};
-  //       fetch.jsonp({
-  //         url: 'https://api.razorpay.com/v1/preferences-wrong?key_id=rzp_test_1DP5mmOlF5G5ag',
-  //         callback: response => {
-  //             const hasValidFields=response.error.description==="Network error";
-  //             if(hasValidFields)
-  //             {
-  //                 done();
-  //             }
-  //             else{
-  //                 done(new Error());
-  //             }
-  //         },
-  //       });
-  //     });
-  // });
+  describe('jsonp', () => {
+    it('Check if it sends jsonp request and gets a response', function(done) {
+      this.timeout(6000);
+
+      // global.Razorpay is needed for the JSDOM Node env
+      global.Razorpay = {};
+
+      // global.window.Razorpay is needed for JSDOM <script> env
+      // and it needs to always be a copy of global.Razorpay as the JSONP functions
+      // will be added to global.Razorpay but the script will expect them to be in global.window.Razorpay
+      global.window.Razorpay = global.Razorpay;
+
+      fetch.jsonp({
+        url:
+          'https://api.razorpay.com/v1/payments/pay_FF48gKluIcsuNv/status?key_id=rzp_test_1DP5mmOlF5G5ag',
+        callback: response => {
+          if (
+            response.http_status_code !== 'undefined' &&
+            response.payment_id !== 'undefined'
+          ) {
+            done();
+          } else {
+            done(
+              new Error(
+                'Got unexpected data in response of fetch.jsonp request'
+              )
+            );
+          }
+        },
+      });
+    });
+  });
 });
